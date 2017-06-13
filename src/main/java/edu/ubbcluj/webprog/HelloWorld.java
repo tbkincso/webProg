b0a89f2bbf94306e7c9ab681a046816a00722aab
Unknown <tuzesbkincso@gmail.com>
2017-06-13 20:13:51 +0300

    [WebProg] Initial project structure

---
+++ src/main/java/edu/ubbcluj/webprog/HelloWorld.java
@@ -0,0 +1,12 @@
+package edu.ubbcluj.webprog;
+
+/**
+ * Created by kincso on 13.06.2017.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Kincso!");
+    }
+
+}
